555dfce081f262ad52a37139b0e4ef0c9e561377
Diegoucm <dimart15@ucm.es>
2024-10-28 10:42:48 +0100

    Vector2d

---
+++ ola/HolaSDL/vector2D.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "checkML.h"
+#include <iostream>
+#include <vector>
+#include "Vector2D.h"
+
+
+template <class T>
+class Vector2D {
+public:
+	T x;
+	T y;
+
+	Vector2D() : x(0), y(0) {}
+	Vector2D(T x, T y) : x(x), y(y) {}
+	Vector2D(const Vector2D<T>& v) : x(v.x), y(v.y) {}
+
+	Vector2D<T> operator+(const Vector2D<T>& v) const {
+		return Vector2D<T>(x + v.x, y + v.y);
+	}
+
+	Vector2D<T> operator-(const Vector2D<T>& v) const {
+		return Vector2D<T>(x - v.x, y - v.y);
+	}
+
+	Vector2D<T> operator*(T n) const {
+		return Vector2D<T>(x * n, y * n);
+	}
+
+	Vector2D<T> operator/(T n) const {
+		return Vector2D<T>(x / n, y / n);
+	}
+
+	Vector2D<T>& operator+=(const Vector2D<T>& v) {
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+
+	Vector2D<T>& operator-=(const Vector2D<T>& v) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+
+	Vector2D<T>& operator*=(T n) {
+		x *= n;
+		y *= n;
+		return *this;
+	}
+
+	Vector2D<T>& operator/=(T n) {
+		x /= n;
+		y /= n;
+		return *this;
+	}
+
+	bool operator==(const Vector2D<T>& v) const {
+		return x == v.x && y == v.y;
+	}
+
+	bool operator!=(const Vector2D<T>& v) const {
+		return x != v.x || y != v.y;
+	}
+
+	bool operator<(const Vector2D<T>& v) const {
+		return x < v.x && y < v.y;
+	}
+
+	bool operator>(const Vector2D<T>& v) const {
+		return x > v.x && y > v.y;
+	}
+
+	bool operator<=(const Vector2D<T>& v) const {
+		return x <= v.x && y <= v.y;
+	}
+
+	bool operator>=(const Vector2D<T>& v) const {
+		return x >= v.x && y >= v.y;
+	}
+
+	Vector2D<T> operator-() const {
+		return Vector2D<T>(
+
+	}
+
+	//getters
+	T getX() const { return x; }
+	T getY() const { return y; }
+
+	//setters
+	void setX(T x) { this->x = x; }
+	void setY(T y) { this->y = y; }
+};
