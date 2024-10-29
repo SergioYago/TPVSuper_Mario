77f4ff5a879578cee44688a487d408439b7739d3
Diegoucm <dimart15@ucm.es>
2024-10-29 12:32:28 +0100

    aaaaaaaaaaa

--- ola/HolaSDL/Game.h
+++ ola/HolaSDL/Game.h
@@ -1,49 +1,65 @@
 #pragma once
+
+// Biblioteca estándar de C++
+#include <array>
+
+// Biblioteca SDL
 #include <SDL.h>
+
+// Nuestras clases
 #include "Texture.h"
-#include <string>
-#include <iostream>
+#include "TileMap.h"
 
-using namespace std;
-using  uint = unsigned int;
-class Game {
+using uint = unsigned int;
 
+//
+// Clase que representa el juego y controla todos sus aspectos
+//
+class Game
+{
 public:
-	static const int TILE_SIDE = 32;  // constantes estáticas en Game
-	static const int WINDOW_WIDTH = 18;
-	static const int WINDOW_HEIGHT = 16;
-	static constexpr uint FRAME_RATE = 50;
-	static const string TEXTURES_PATH;
-	
-	int mapOffset;
-	// atributo de Game
-	Game();
-
-
+	// Identificadores de las texturas
 	enum TextureName {
 		BACKGROUND,
-		TILEMAP,
+		PLAYER,
 		NUM_TEXTURES,  // Truco C++: número de texturas definidas
 	};
-	static Texture* cosas[NUM_TEXTURES];
 
 private:
-	
-	
-	
-	
 	// Ventana de la SDL (se destruirá en el destructor)
 	SDL_Window* window = nullptr;
 	// Renderizador de la SDL (para dibujar)
 	SDL_Renderer* renderer = nullptr;
-	 
-	
-	
 	// Array con todas las texturas del juego
-	//std::array<Texture*, NUM_TEXTURES> ;
+	std::array<Texture*, NUM_TEXTURES> textures;
+	// Interruptor para terminar el juego
+	bool seguir;
+	int mapOffset;
 
-	void Añadir()
-	{
-		//cosas[1]= Texture(renderer, "../assets/imgs/blocks.png", 1, 1);
-	}
+	// Objetos del juego
+	TileMap* tilemap;
+
+public:
+	void run();
+
+	void update();
+	void render() const;
+	void handleEvents();
+
+	Texture* getTexture(TextureName name) const;
+
+	// Constante globales
+	static constexpr uint WIN_WIDTH = 576;
+	static constexpr uint WIN_HEIGHT = 512;
+	static constexpr uint FRAME_RATE = 50;
+	static constexpr uint TILE_SIDE = 32;
+
+	Game();
+	~Game();
 };
+
+inline Texture*
+Game::getTexture(TextureName name) const
+{
+	return textures[name];
+}
